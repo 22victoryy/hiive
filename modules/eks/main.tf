73cfbf0d9c276cf4e6aa2dc76909bdf3fdaa2fa8
22victoryy <dk90801@gmail.com>
2025-06-30 05:04:27 -0400

    fix eks main tf syntax error

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,31 +1,34 @@
-# Simple EKS module to handle underlying configurations with the latest version available
 
+# Syntax correction
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  source = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
   cluster_name    = var.name
   cluster_version = "1.29"
-  subnet_ids      = var.subnet_ids
-  vpc_id          = var.vpc_id
 
-  # make cluster accessible via endpoint on local machine. In prod, should probbaly add a VPN or do some type of VPC peering
+  # provide vpc and subnet ids
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  # allow cluster access via URL
   cluster_endpoint_public_access = true
 
-  # keep it very simple one node group with max size 2 
+  # Fetch existing IAM role through arn
+  create_iam_role = false
+  iam_role_arn    = var.cluster_iam_role_arn
+
+ # t3.small 2 cluster, keeping it simple
+ # consumed resources and money, shoudl not be run overnight
   eks_managed_node_groups = {
     hiive-cluster = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
       instance_types = ["t3.small"]
-
-      iam_role_arn = var.node_iam_role_arn
+      iam_role_arn   = var.node_iam_role_arn
     }
   }
-
-  cluster_iam_role_arn = var.cluster_iam_role_arn
 
   tags = {
     Environment = "dev"
